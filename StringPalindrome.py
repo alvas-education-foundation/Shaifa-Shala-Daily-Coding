f565cab3cce6b3661d2dc26eb59ab26c65b2b82f
shaifashala <53564301+shaifashala@users.noreply.github.com>
2020-06-08 16:12:24 +0300

    Update StringPalindrome.py

--- StringPalindrome.py
+++ StringPalindrome.py
@@ -1,6 +1,9 @@
 /*Write a python function that will take a string and checks whether it is a palindrome or not. Return If it a palindrome, print true else print false*/
+def palindromecheck(string):
+    if(string == string[:: - 1]):
+        print("True")
+    else:
+        print("False")
+#main
 string = input("Enter the String : ")
-if(string == string[:: - 1]):
-   print("True")
-else:
-   print("False")
+palindromecheck(string)
