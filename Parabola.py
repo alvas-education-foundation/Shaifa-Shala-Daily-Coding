03177d9beb8b43c4f26dc0af8b29c7d0a327ea06
shaifashala <53564301+shaifashala@users.noreply.github.com>
2020-08-04 14:50:36 +0300

    Create Parabola.py

---
+++ Parabola.py
@@ -0,0 +1,13 @@
+/*Python Program for Finding the vertex, focus and directrix of a parabola*/
+def parabola(a, b, c):
+    print ("Vertex: (" , (-b / (2 * a)) , ", "
+        ,(((4 * a * c) - (b * b)) / (4 * a)) , ")" )
+    print ("Focus: (" , (-b / (2 * a)) , ", "
+        , (((4 * a * c) - (b * b) + 1) / (4 * a)) , ")" )
+    print ("Directrix: y="
+            , (int)(c - ((b * b) + 1) * 4 * a ))
+# main
+a = 5
+b = 3
+c = 2
+parabola(a, b, c)
