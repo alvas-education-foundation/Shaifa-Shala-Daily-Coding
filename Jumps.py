054f801fe32af5c9f87c55a523e27c01016cbdca
shaifashala <53564301+shaifashala@users.noreply.github.com>
2020-08-05 15:32:22 +0300

    Create Jumps.py

---
+++ Jumps.py
@@ -0,0 +1,21 @@
+/*Python Program for Number of jump required of given length to reach a point of form (d, 0) from origin in 2D plane*/
+def minJumps(a, b, d):
+    temp = a
+    a = min(a, b)
+    b = max(temp, b)
+    if (d >= b):
+        return (d + b - 1) / b
+    # if d is 0
+    if (d == 0):
+        return 0
+    # if d is equal to a.
+    if (d == a):
+        return 1
+    # else make triangle, and only 2  
+    # steps required.
+    return 2
+# main()
+a = 3
+b = 4
+d = 11
+print (int(minJumps(a, b, d)))
